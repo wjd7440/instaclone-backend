e1523f8b48e3b90541c7ee539002bc88c0d59ef1
jungstar <50983731+wjd7440@users.noreply.github.com>
2021-05-08 16:38:37 +0900

    2021-05-08 16:38

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
+import pkg from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
 
+const { PrismaClient } = pkg;
+const client = new PrismaClient();
+
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+    genre: String
+    createdAt: String
+    updatedAt: String
+  }
+
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "vev",
+    movies: () => client.movie.findMany(),
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_, { title }) => {
+      console.log(args);
+      return true;
+    },
   },
 };
 
