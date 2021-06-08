21aead4e6177db74b5c51f709196787f7d821377
jungstar <50983731+wjd7440@users.noreply.github.com>
2021-06-08 23:11:44 +0900

    2021-06-08

--- photos/photos.utils.js
+++ photos/photos.utils.js
@@ -1,5 +1,5 @@
 export const processHashtags = (caption) => {
-  const hashtags = caption.match(/#[\w]+/g) || [];
+  const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g) || [];
   return hashtags.map((hashtag) => ({
     where: { hashtag },
     create: { hashtag },
